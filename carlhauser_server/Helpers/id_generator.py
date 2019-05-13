25993c1680692f0405004b2ad332995540584bec
Vincent-CIRCL <zettacircl@protonmail.com>
2019-05-13 14:52:39 +0200

    add: [core] Tests path issue solved + Test added + BMP conversion + SHA1 hash

--- carlhauser_server/Helpers/id_generator.py
+++ carlhauser_server/Helpers/id_generator.py
@@ -2,7 +2,37 @@
 # -*- coding: utf-8 -*-
 
 # ==================== ------ STD LIBRARIES ------- ====================
-import sys, os
-
+import sys, os, io
+from PIL import Image
+import hashlib
+import pathlib
 # ==================== ------ PERSONAL LIBRARIES ------- ====================
 sys.path.append(os.path.abspath(os.path.pardir))
+
+
+def get_SHA1(file_value : io.BufferedReader):
+    # Get SHA1 hash from the file, directly in memory
+
+    h  = hashlib.sha1()
+    b  = bytearray(128*1024)
+    mv = memoryview(b)
+
+    for n in iter(lambda : file_value.readinto(mv), 0):
+        h.update(mv[:n])
+
+    return h.hexdigest()
+
+
+def convert_to_bmp(file_value : io.BufferedReader):
+    # Get SHA1 hash from the file, directly in memory
+
+    curr_img = Image.open(file_value)
+    with io.BytesIO() as output:
+        curr_img.save(output, format="BMP")
+        contents = output.getvalue()
+        return contents
+
+def write_to_file(file_value, file_path : pathlib.Path):
+    newfile = open(str(file_path), 'wb')
+    newfile.write(file_value)
+    newfile.close()
