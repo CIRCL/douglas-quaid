f7ecada23d285cf55e43cc8a5e84dbeeb0b09c99
Vincent-CIRCL <zettacircl@protonmail.com>
2019-06-14 09:25:08 +0200

    add: [core] Fixing classification launch script

--- datasets/HumanHash/classifier_launch_client.sh
+++ datasets/HumanHash/classifier_launch_client.sh
@@ -3,9 +3,12 @@
 set -e
 set -x
 
-echo ‘Establishing proxy ’
-ssh -D 8080 crdb
+echo ‘Establishing proxy. Please leave the screen with "CTRL+A+D" ’
+screen -dm -S Proxy_ssh_to_CRDB ssh -D 8080 crdb
 echo ‘Please configure proxy in your default browser and then press key to continue.’
 read -p "Press any key to continue... " -n1 -s
-echo ‘Accessing website ...'
-xdg-open ‘http://149.13.33.83:8080’
+echo 'Accessing website ...'
+xdg-open 'http://149.13.33.83:80'
+echo ‘If you then want to kill the proxy .. press a key’
+read -p "Press any key to continue... " -n1 -s
+screen -X -S Proxy_ssh_to_CRDB quit
--- datasets/HumanHash/classifier_launcher_server.sh
+++ datasets/HumanHash/classifier_launcher_server.sh
@@ -6,10 +6,13 @@ set -x
 echo ‘Launching docker service ’
 sudo service docker start
 echo ‘Current working and old dockers : ’
-docker ps -a
+sudo docker ps -a
 echo ‘Please check if no docker is already running on port 80’
 read -p "Press any key to continue... " -n1 -s
 echo ‘Launching docker’
-screen -S docker_classifcation sudo docker run -d -p 80:80 dataturks/dataturks:3.3.0 --name=classification_docker
+screen -dm -S docker_classifcation sudo docker run -d -p 80:80 dataturks/dataturks:3.3.0 --name=classification_docker
+cd /home/vincent/AIL_dataset_checked/
+echo ‘Serving pictures'
+screen -dm -S picture_serving_python python -m SimpleHTTPServer 8000
 echo ‘Please proxy on : ’
 ip a | grep "scope global" | grep -Po '(?<=inet )[\d.]+'
