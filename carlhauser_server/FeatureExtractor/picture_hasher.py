b583e68c37929e16b7f2f31ced1b251f48562c2e
Vincent-CIRCL <zettacircl@protonmail.com>
2019-06-18 15:42:57 +0200

    add: [core] Weighted mean for algorithm merging

--- carlhauser_server/FeatureExtractor/picture_hasher.py
+++ carlhauser_server/FeatureExtractor/picture_hasher.py
@@ -36,25 +36,25 @@ class Picture_Hasher():
 
         try:
             # Note : @image must be a PIL instance.
-            if self.fe_conf.A_HASH.get("is_enabled",False):
+            if self.fe_conf.A_HASH.get("is_enabled", False):
                 self.logger.debug("A-HASH ... ")
                 answer["A_HASH"] = self.check_null_hash(imagehash.average_hash(pil_picture))
-            if self.fe_conf.P_HASH.get("is_enabled",False):
+            if self.fe_conf.P_HASH.get("is_enabled", False):
                 self.logger.debug("P_HASH ... ")
                 answer["P_HASH"] = self.check_null_hash(imagehash.phash(pil_picture))
-            if self.fe_conf.P_HASH_SIMPLE.get("is_enabled",False):
+            if self.fe_conf.P_HASH_SIMPLE.get("is_enabled", False):
                 self.logger.debug("P_HASH_SIMPLE ... ")
                 answer["P_HASH_SIMPLE"] = self.check_null_hash(imagehash.phash_simple(pil_picture))
-            if self.fe_conf.D_HASH.get("is_enabled",False):
+            if self.fe_conf.D_HASH.get("is_enabled", False):
                 self.logger.debug("D_HASH ... ")
                 answer["D_HASH"] = self.check_null_hash(imagehash.dhash(pil_picture))
-            if self.fe_conf.D_HASH_VERTICAL.get("is_enabled",False):
+            if self.fe_conf.D_HASH_VERTICAL.get("is_enabled", False):
                 self.logger.debug("D_HASH_VERTICAL ... ")
                 answer["D_HASH_VERTICAL"] = self.check_null_hash(imagehash.dhash_vertical(pil_picture))
-            if self.fe_conf.W_HASH.get("is_enabled",False):
+            if self.fe_conf.W_HASH.get("is_enabled", False):
                 self.logger.debug("W_HASH ... ")
                 answer["W_HASH"] = self.check_null_hash(imagehash.whash(pil_picture))
-            if self.fe_conf.TLSH.get("is_enabled",False):
+            if self.fe_conf.TLSH.get("is_enabled", False):
                 self.logger.debug("TLSH ... ")
                 answer["TLSH"] = self.check_null_hash(tlsh.hash(curr_picture))
 
@@ -67,6 +67,6 @@ class Picture_Hasher():
         # Check if the hash provided is null/None/empty. If yes, provide a default hash
         self.logger.debug("Checking hash ... ")
 
-        if not hash :
+        if not hash:
             return '0000000000000000000000000000000000000000000000000000000000000000000000'
         return hash
