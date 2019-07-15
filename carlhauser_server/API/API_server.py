e6e64f5fb0ca6ea1f2baa441c75d73a3448adcc8
Vincent-CIRCL <zettacircl@protonmail.com>
2019-07-15 08:42:04 +0200

    chg: [core] refractor and factorzation server api

--- carlhauser_server/API/API_server.py
+++ carlhauser_server/API/API_server.py
@@ -5,20 +5,22 @@
 # Inspired from : https://github.com/D4-project/IPASN-History/blob/master/website/web/__init__.py
 # If you are derouted by the lack of decorator, go there : https://stackoverflow.com/questions/17129573/can-i-use-external-methods-as-route-decorators-in-python-flask
 
+# ==================== ------ STD LIBRARIES ------- ====================
+import flask
+
 import argparse
 import logging
 import os
 import pathlib
 import sys
 import time
-
-# ==================== ------ STD LIBRARIES ------- ====================
-import flask
+from typing import Dict
+import werkzeug.datastructures as datastructures
 
 # ==================== ------ PERSONAL LIBRARIES ------- ====================
-sys.path.append(os.path.abspath(os.path.pardir))
 
 from common.environment_variable import get_homedir, dir_path
+from common.environment_variable import QueueNames, EndPoints
 
 import carlhauser_server.Configuration.webservice_conf as webservice_conf
 import carlhauser_server.Configuration.database_conf as database_conf
@@ -29,18 +31,34 @@ import carlhauser_server.DatabaseAccessor.database_worker as database_worker
 
 import common.ImportExport.json_import_export as json_import_export
 import carlhauser_server.DatabaseAccessor.database_utilities as db_utils
-from carlhauser_server.Configuration.static_values import QueueNames
+
+sys.path.append(os.path.abspath(os.path.pardir))
 
 
 # ==================== ------ SERVER Flask API definition ------- ====================
 
 class EndpointAction(object):
+    '''
+    Defines an Endpoint for a specific action for any client.
+    '''
 
     def __init__(self, action):
+        '''
+        Create the endpoint by specifying which action we want the endpoint to perform, at each call
+        :param action: The function to execute on endpoint call
+        '''
+        # Defines which action (which function) should be called
         self.action = action
-        # self.response = flask.Response("Welcome to carl-hauser", status=200, headers={})
+
+        # DEBUG FOR TEST PURPOSES # self.response = flask.Response("Welcome to carl-hauser", status=200, headers={})
 
     def __call__(self, *args):
+        '''
+        Standard method that effectively perform the stored action of this endpoint.
+        :param args: Arguments to give to the stored function
+        :return: The response, which is a jsonified version of the function returned value
+        '''
+
         # Perform the action
         answer = self.action(*args)
 
@@ -57,19 +75,24 @@ class EndpointAction(object):
 
 
 class FlaskAppWrapper(object):
-    def __init__(self, name, ws_conf: webservice_conf, db_conf: database_conf):
+    def __init__(self, name: str, ws_conf: webservice_conf, db_conf: database_conf):
         # STD attributes
         self.conf = ws_conf
         self.logger = logging.getLogger(__name__)
 
         # Specific attributes
         self.app = flask.Flask(name)
+
         # An accessor to push stuff in queues, mainly
         self.database_worker = database_worker.Database_Worker(db_conf=db_conf)
         self.db_utils = None
 
     def run(self):
-        # Handle SLL Certificate, if they are provided = use them, else = self sign a certificate on the fly
+        '''
+        Final action to call to run the flask process, which generate endpoints, and so.
+        Handle SLL Certificate, if they are provided = use them, else = self sign a certificate on the fly
+        :return: no return (continuously executing. Return error code if stopped and so)
+        '''
         if self.conf.CERT_FILE is None or self.conf.KEY_FILE is None:
             self.logger.error(f"Provided CERT OR KEY file not found :  {self.conf.CERT_FILE} and {self.conf.KEY_FILE}")
             self.app.run(ssl_context='adhoc')
@@ -78,22 +101,46 @@ class FlaskAppWrapper(object):
             self.app.run(ssl_context=(str(self.conf.CERT_FILE), str(self.conf.KEY_FILE)))  # ssl_context='adhoc')
 
     def add_all_endpoints(self):
+        '''
+        Add all endpoints = all callable URL of the API, and link them to the good functions
+        :return: Nothing
+        '''
+
         # Add root endpoint
-        self.add_endpoint(endpoint="/", endpoint_name="/", handler=self.ping)
+        tmp_ep_name = "/"
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.ping)
 
         # Add action endpoints
-        self.add_endpoint(endpoint="/add_picture", endpoint_name="/add_picture", handler=self.add_picture)
-        self.add_endpoint(endpoint="/request_similar_picture", endpoint_name="/request_similar_picture", handler=self.request_similar_picture)
-        self.add_endpoint(endpoint="/is_ready", endpoint_name="/is_ready", handler=self.is_ready)
-        self.add_endpoint(endpoint="/get_results", endpoint_name="/get_results", handler=self.get_results)
-        self.add_endpoint(endpoint="/export_db", endpoint_name="/export_db", handler=self.export_db_as_graphe)
+        tmp_ep_name = "/" + EndPoints.ADD_PICTURE,
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.add_picture)
+        tmp_ep_name = "/" + EndPoints.REQUEST_PICTURE,
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.request_similar_picture)
+        tmp_ep_name = "/" + EndPoints.WAIT_FOR_REQUEST,
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.is_ready)
+        tmp_ep_name = "/" + EndPoints.GET_REQUEST_RESULT,
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.get_results)
+        tmp_ep_name = "/" + EndPoints.REQUEST_DB,
+        self.add_endpoint(endpoint=tmp_ep_name, endpoint_name=tmp_ep_name, handler=self.export_db_as_graphe)
 
     def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None):
+        '''
+        Add one endpoint to the flask application. Accept GET, POST and PUT.
+        :param endpoint: Callable URL.
+        :param endpoint_name: Name of the Endpoint
+        :param handler: function to execute on call on the URL
+        :return: Nothing
+        '''
         self.app.add_url_rule(endpoint, endpoint_name, EndpointAction(handler), methods=['GET', 'POST', 'PUT'])
 
     # ==================== ------ API Calls ------- ====================
 
-    def ping(self, *args):
+    def ping(self, *args) -> Dict:
+        '''
+        Handle a ping request on server side. Creates a pong answer
+        :param args: None is required
+        :return: The result json (pong)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "ping"
         result_json = self.add_std_info(result_json)
@@ -104,45 +151,42 @@ class FlaskAppWrapper(object):
         return result_json
         # Test it with curl 127.0.0.1:5000
 
-    def add_picture(self):
+    def add_picture(self) -> Dict:
+        '''
+        Handle an adding of a picture request on server side.
+        :return: The result json (status of the request, etc.)
+        '''
         result_json = {}
-        result_json["Called_function"] = "add_picture"
+        result_json["Called_function"] = EndPoints.ADD_PICTURE
         result_json = self.add_std_info(result_json)
 
-        # Answer to PUT HTTP request
+        # Answer ONLY to PUT HTTP request
         if flask.request.method == 'PUT':
             try:
                 # Received : werkzeug.datastructures.FileStorage. Should use ".read()" to get picture's value
                 f = flask.request.files['image']
-                self.logger.debug(f"Image received in server : {type(f)} ")  # {f.read()}
 
-                # Compute input picture hash and convert to BMP
-                f_hash = id_generator.get_SHA1(f)
-                f_bmp = id_generator.convert_to_bmp(f)  # Returns a bytes array
-                self.logger.debug(f"Image transformed in BMP in server : {type(f_bmp)} ")  # {f_bmp}
+                # Convert, save and send picture to server
+                result_json = self.enqueue_and_save_picture(file=f,
+                                                            result_json=result_json,
+                                                            queue=QueueNames.FEATURE_TO_ADD)
 
-                # Save received picture to disk
-                picture_import_export.save_picture(f_bmp, get_homedir() / 'datasets' / 'received_pictures' / (str(f_hash) + '.bmp'))
-                # If the filename need to be used : secure_filename(f.filename)
-
-                # Enqueue picture to processing
-                self.logger.debug(f"Adding to feature queue : {f_hash} ")  # {f_bmp}
-                self.database_worker.add_to_queue(self.database_worker.cache_db_decode, queue_name=QueueNames.FEATURE_TO_ADD, id=f_hash, dict_to_store={"img": f_bmp})
-
-                result_json["Status"] = "Success"
-                result_json["img_id"] = f_hash
             except Exception as e:
                 self.logger.error(f"Error during PUT handling {e}")
                 result_json["Status"] = "Failure"
                 result_json["Error"] = "Error during Hash computation or database adding"
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use PUT instead of GET, POST, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="PUT")
 
         return result_json
         # Test it with curl 127.0.0.1:5000/add_pict
 
-    def are_pipelines_empty(self):
+    def are_pipelines_empty(self) -> Dict:
+        '''
+        Handle a check of emptiness on the pipelines (list of queues)
+        :return: The result json (status of the request, etc.)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "are_pipelines_empty"
         result_json = self.add_std_info(result_json)
@@ -160,13 +204,17 @@ class FlaskAppWrapper(object):
                 result_json["Status"] = "Failure"
                 result_json["Error"] = "Error during database request"
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use GET instead of POST, PUT, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="GET")
 
         return result_json
         # Test it with curl 127.0.0.1:5000/are_pipelines_empty
 
-    def request_similar_picture(self):
+    def request_similar_picture(self) -> Dict:
+        '''
+        Handle a request of one picture, to return a list of similar pictures
+        :return: The result json (status of the request, etc.)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "request_similar_picture"
         result_json = self.add_std_info(result_json)
@@ -176,37 +224,28 @@ class FlaskAppWrapper(object):
             try:
                 # Received : werkzeug.datastructures.FileStorage. Should use ".read()" to get picture's value
                 f = flask.request.files['image']
-                self.logger.debug(f"Image received in server : {type(f)} ")  # {f.read()}
 
-                # Compute input picture hash and convert to BMP
-                f_hash = id_generator.get_SHA1(f)
-                f_bmp = id_generator.convert_to_bmp(f)  # Returns a bytes array
-                self.logger.debug(f"Image transformed in BMP in server : {type(f_bmp)} ")  # {f_bmp}
+                # Convert, save and send picture to server
+                result_json = self.enqueue_and_save_picture(file=f,
+                                                            result_json=result_json,
+                                                            queue=QueueNames.FEATURE_TO_REQUEST)
 
-                # Save received picture to disk
-                picture_import_export.save_picture(f_bmp, get_homedir() / 'datasets' / 'received_pictures' / (str(f_hash) + '.bmp'))
-                # If the filename need to be used : secure_filename(f.filename)
-
-                # TODO : Create request UUID ? Or keep image hash ?
-
-                # Enqueue picture to processing
-                self.logger.debug(f"Adding to feature queue : {f_hash} ")  # {f_bmp}
-                self.database_worker.add_to_queue(self.database_worker.cache_db_decode, queue_name=QueueNames.FEATURE_TO_REQUEST, id=f_hash, dict_to_store={"img": f_bmp})
-
-                result_json["Status"] = "Success"
-                result_json["request_id"] = f_hash
             except Exception as e:
                 self.logger.error(f"Error during PUT handling {e}")
                 result_json["Status"] = "Failure"
                 result_json["Error"] = "Error during Hash computation or database request"
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use POST instead of GET, PUT, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="POST")
 
         return result_json
         # Test it with curl 127.0.0.1:5000/request_similar_picture
 
-    def is_ready(self):
+    def is_ready(self) -> Dict:
+        '''
+        Handle a check of a request readiness
+        :return: The result json (status of the request, etc.)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "is_ready"
         result_json = self.add_std_info(result_json)
@@ -231,13 +270,17 @@ class FlaskAppWrapper(object):
                 result_json["Error"] = "Error during database request"
                 result_json["is_ready"] = False
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use GET instead of POST, PUT, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="GET")
 
         return result_json
         # Test it with curl 127.0.0.1:5000/is_ready
 
-    def get_results(self):
+    def get_results(self) -> Dict:
+        '''
+        Handle a retrieval of results
+        :return: The result json (status of the request, etc.)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "get_results"
         result_json = self.add_std_info(result_json)
@@ -260,13 +303,17 @@ class FlaskAppWrapper(object):
                 result_json["Status"] = "Failure"
                 result_json["Error"] = "Error during Hash computation or database request"
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use GET instead of POST, PUT, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="GET")
 
         return result_json
         # Test it with curl 127.0.0.1:5000/get_results
 
-    def export_db_as_graphe(self):
+    def export_db_as_graphe(self) -> Dict:
+        '''
+        Handle a dump of the database request
+        :return: The result json (status of the request, etc.)
+        '''
+
         result_json = {}
         result_json["Called_function"] = "export_db"
         result_json = self.add_std_info(result_json)
@@ -288,16 +335,78 @@ class FlaskAppWrapper(object):
                 result_json["Status"] = "Failure"
                 result_json["Error"] = "Error during db exportation to file"
         else:
-            result_json["Status"] = "Failure"
-            result_json["Error"] = "BAD METHOD : use POST instead of GET, PUT, ..."
+            result_json = self.add_bad_method_info(result_json, good_method_instead="GET")
 
         return result_json
         # Test it with curl 127.0.0.1:5000
 
     @staticmethod
-    def add_std_info(result_json):
+    def add_std_info(result_json: Dict) -> Dict:
+        '''
+        Add standard information to the result_json
+        :param result_json: The result json/dict to which to add standard values
+        :return: the modified result_json
+        '''
         result_json["Call_method"] = flask.request.method  # 'GET' or 'POST' ...
         result_json["Call_time"] = time.ctime()  # 'GET' or 'POST' ...
+
+        return result_json
+
+    @staticmethod
+    def add_bad_method_info(result_json: Dict, good_method_instead: str = None) -> Dict:
+        '''
+        Add error values to the result_json if method is bad
+        :param result_json: The result json/dict to which to add standard values
+        :param good_method_instead: the method that should be used by the resquest
+        :return: the modified result_json
+        '''
+        result_json["Status"] = "Failure"
+
+        # Construct a message depending on the good method to use
+        if good_method_instead is None:
+            result_json["Error"] = "BAD METHOD : use another method (GET, PUT, POST, ...)."
+
+        elif good_method_instead == "POST":
+            result_json["Error"] = "BAD METHOD : use POST instead of GET, PUT, ..."
+
+        elif good_method_instead == "PUT":
+            result_json["Error"] = "BAD METHOD : use PUT instead of GET, POST, ..."
+
+        elif good_method_instead == "GET":
+            result_json["Error"] = "BAD METHOD : use GET instead of POST, PUT, ..."
+
+        return result_json
+
+    def enqueue_and_save_picture(self, file: datastructures.FileStorage, result_json: Dict, queue: QueueNames) -> Dict:
+        '''
+        Hash the picture, generate a BMP file, enqueue it, return the result_json with relevant values
+        :param file: the file provided by the client
+        :param result_json: The result json/dict to which to add standard values
+        :return: the modified result_json
+        '''
+        # Received : werkzeug.datastructures.FileStorage. Should use ".read()" to get picture's value
+        self.logger.debug(f"Image received in server : {type(file)} ")  # {f.read()}
+
+        # Compute input picture hash and convert to BMP
+        f_hash = id_generator.get_SHA1(file)
+        f_bmp = id_generator.convert_to_bmp(file)  # Returns a bytes array
+        self.logger.debug(f"Image transformed in BMP in server : {type(f_bmp)} ")  # {f_bmp}
+
+        # Save received picture to disk
+        picture_import_export.save_picture(f_bmp, get_homedir() / 'datasets' / 'received_pictures' / (str(f_hash) + '.bmp'))
+        # If the filename need to be used : secure_filename(f.filename)
+
+        # TODO : Create request UUID ? Or keep image hash ?
+
+        # Enqueue picture to processing
+        self.logger.debug(f"Adding to feature queue : {f_hash} ")  # {f_bmp}
+        self.database_worker.add_to_queue(self.database_worker.cache_db_decode,
+                                          queue_name=queue,
+                                          id=f_hash,
+                                          dict_to_store={"img": f_bmp})
+
+        result_json["Status"] = "Success"
+        result_json["request_id"] = f_hash
 
         return result_json
 
